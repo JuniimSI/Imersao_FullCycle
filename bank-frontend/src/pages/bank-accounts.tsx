413a55a49288fb3b86624efb9cb9bb0991c689d6
JuniimSI <jrfernandesti@gmail.com>
2021-02-07 19:11:22 -0300

    Lista de contas bancarias testadas

--- bank-frontend/src/pages/bank-accounts.tsx
+++ bank-frontend/src/pages/bank-accounts.tsx
@@ -1,30 +1,41 @@
 // @flow 
 import * as React from 'react';
+import { GetServerSideProps, NextPage } from "next";
 import BankAccountCard from '../components/BankAccountCard';
 import Layout from '../components/Layout';
 import Title from '../components/Title';
-type Props = {
+import { BankAccount } from '../model';
+import { bankHttp } from '../util/http';
 
-};
+interface BankAccountsListProps {
+    bankAccounts: BankAccount[];
+}
 
-const BankAccountList = (props: Props) => {
+const BankAccountList: NextPage<BankAccountsListProps> = (props) => {
+
+    const { bankAccounts } = props;
+
     return (
         <Layout>
             <Title>Contas bancárias</Title>
             <div className="row">
-                <a href="" className="col-12 col-sm-6 col-md4">
-                    <BankAccountCard bankAccount={{
-                        id: 'teste', owner_name: 'teste1', balance: 0, account_number: '1111'
-                    }}/>
-                </a>
-                <a href="" className="col-12 col-sm-6 col-md4">
-                    <BankAccountCard bankAccount={{
-                        id: 'teste2', owner_name: 'teste2', balance: 0, account_number: '2222'
-                    }}/>
-                </a>
+                {bankAccounts.map((b, key) => (
+                    <a key={key} className="col-12 col-sm-6 col-md4">
+                        <BankAccountCard bankAccount={b} />
+                    </a>
+                ))}
             </div>
         </Layout>
     );
 };
 
 export default BankAccountList;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const { data: bankAccounts } = await bankHttp.get("bank-accounts");
+    return {
+        props: {
+            bankAccounts,
+        },
+    };
+};
