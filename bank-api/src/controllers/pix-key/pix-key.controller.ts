fcc48cc8eefdb6c756e4829f64855cad6aa24e6a
JuniimSI <jrfernandesti@gmail.com>
2021-02-06 22:21:39 -0300

    first 201 created pixkey

--- bank-api/src/controllers/pix-key/pix-key.controller.ts
+++ bank-api/src/controllers/pix-key/pix-key.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Inject, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, ParseUUIDPipe, Post, ValidationPipe } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PixKeyDto } from 'src/dto/pix-key.dto';
 import { BankAccount } from 'src/models/bank-account.model';
 import { PixKey } from 'src/models/pix-key.model';
 import { Repository } from 'typeorm';
@@ -33,8 +34,13 @@ export class PixKeyController {
 
 
     @Post()
-    store(){
-
+    async store(
+      @Param('bankAccountId', new ParseUUIDPipe({ version: '4' }))
+      bankAccountId: string,
+      @Body(new ValidationPipe({ errorHttpStatusCode: 422 }))
+      body: PixKeyDto,
+    ) {
+       await this.bankAccountRepo.findOneOrFail(bankAccountId);
     }
 
     @Get('exists')
