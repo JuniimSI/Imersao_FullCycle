511efd50d2b175ef83cabcfeb8bda3d0d6456273
JuniimSI <jrfernandesti@gmail.com>
2021-02-06 23:12:46 -0300

    Adicionando até o a config do pg admin

--- bank-api/src/controllers/pix-key/pix-key.controller.ts
+++ bank-api/src/controllers/pix-key/pix-key.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Inject, Param, ParseUUIDPipe, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Inject, InternalServerErrorException, Param, ParseUUIDPipe, Post, UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PixKeyDto } from 'src/dto/pix-key.dto';
 import { BankAccount } from 'src/models/bank-account.model';
 import { PixKey } from 'src/models/pix-key.model';
 import { Repository } from 'typeorm';
+import { PixService } from 'src/grpc-types/pix-service.grpc';
 
 @Controller('bank-accounts/:bankAccountId/pix-keys')
 export class PixKeyController {
@@ -13,8 +15,8 @@ export class PixKeyController {
         private pixKeyRepo: Repository<PixKey>,
         @InjectRepository(BankAccount)
         private bankAccountRepo: Repository<BankAccount>,
-       // @Inject('CODEPIX_PACKAGE')
-       // private client: ClientGrpc,
+        @Inject('CODEPIX_PACKAGE')
+        private client: ClientGrpc,
       ) {}
       
     @Get()
@@ -41,6 +43,45 @@ export class PixKeyController {
       body: PixKeyDto,
     ) {
        await this.bankAccountRepo.findOneOrFail(bankAccountId);
+       
+       const pixService: PixService = this.client.getService('PixService');
+      
+       const notFound = await this.checkPixKeyNotFound(body);
+       if (!notFound) {
+         throw new UnprocessableEntityException('PixKey already exists');
+       }
+   
+       const createdPixKey = await pixService
+         .registerPixKey({
+           ...body,
+           accountId: bankAccountId,
+         })
+         .toPromise();
+   
+       if (createdPixKey.error) {
+         throw new InternalServerErrorException(createdPixKey.error);
+       }
+       
+       const pixKey = this.pixKeyRepo.create({
+        id: createdPixKey.id,
+        bank_account_id: bankAccountId,
+        ...body,
+      });
+      return await this.pixKeyRepo.save(pixKey);
+    }
+
+    async checkPixKeyNotFound(params: { key: string; kind: string }) {
+      const pixService: PixService = this.client.getService('PixService');
+      try {
+        await pixService.find(params).toPromise();
+        return false;
+      } catch (e) {
+        if (e.details === 'no key was found') {
+          return true;
+        }
+  
+        throw new InternalServerErrorException('Server not available');
+      }
     }
 
     @Get('exists')
